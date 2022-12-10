0dc73799a4396a428dcde30b92c8f85cadf8b72c
omguptalko <119674842+omguptalko@users.noreply.github.com>
2022-12-10 14:54:31 +0530

    Add files via upload

---
+++ coun.c
@@ -0,0 +1,16 @@
+// Program counting the number 
+#include<stdio.h>
+void main( )
+{
+ int x , boy ,girl ;
+
+printf(" the number of boys  ");
+scanf("%d",&x);
+if(x ==1)
+boy = boy +1 ;
+
+
+if ( x ==2)
+girl =girl +1 ;
+printf( "total number of boy %d\n   girl %d\n",boy , girl);
+}      
